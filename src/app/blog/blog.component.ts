850ac4c52a93d8d13a877139bf571f11f9acc134
jyotinamdeo95@gmail.com <jyotinamdeo95@gmail.com>
2020-05-29 01:10:03 +0530

    Gallery page implemented

--- src/app/blog/blog.component.ts
+++ src/app/blog/blog.component.ts
@@ -12,9 +12,17 @@ export class BlogComponent implements OnInit {
   BLOG_VIDEO: string = "assets/images/gifs/nebula.mp4";
 
 
+  Black_white_gallery:any[]=["assets/images/gallery/Black-white/Deves.jpg","assets/images/gallery/Black-white/Kovind.jpg","assets/images/gallery/Black-white/Prachi.jpg",
+  "assets/images/gallery/Black-white/Radhe-Krishna.jpg","assets/images/gallery/Black-white/A-Girl.jpg","assets/images/gallery/Black-white/Vinchi.jpg","assets/images/gallery/Black-white/A-Letter.png","assets/images/gallery/Black-white/S-Letter.jpg" ];
+  colored_gallery:any[]=[
+    "assets/images/gallery/colored/Lord-ganesha.jpg","assets/images/gallery/colored/Ankita.jpg","assets/images/gallery/colored/Couple.jpg","assets/images/gallery/colored/Red-rose.png"
+  ];
+
+
   constructor() { }
 
   ngOnInit() {
+
   }
 
 }
